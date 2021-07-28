6be492663cf435666345470fbaeb6099298bf5a2
LocNguyen2000 <lolkun2000@gmail.com>
2021-07-28 16:28:07 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Connect to Postgres DB
+
+
+const userRoutes = require("./routes/user");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use("/user", userRoutes);
+
+app.get("/", async (req: Request, res: Response) => {
+  res.status(200).send({
+    message: "Hello World!",
+  });
+});
+
+try {
+  app.listen(port, (): void => {
+    console.log(`Connected successfully on port ${port}`);
+  });
+} catch (error) {
+  console.error(`Error occured: ${error.message}`);
+}
